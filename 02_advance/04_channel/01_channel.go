6243b742f2a524345c243de069ad5ac72eaa2563
meetbetter@foxmail.com <35328917+meetbetter@users.noreply.github.com>
2019-07-28 14:36:24 +0800

    Create 01_channel.go

---
+++ 02_advance/04_channel/01_channel.go
@@ -0,0 +1,32 @@
+package main
+
+/*
+	程序说明：
+	演示channel的作用之一：传输数据。
+*/
+
+import "fmt"
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
+}
