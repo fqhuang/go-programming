8f1650d8222b354f33196e2f206b553a2229de28
meetbetter@foxmail.com <35328917+meetbetter@users.noreply.github.com>
2019-07-31 21:27:21 +0800

    Update 11_自己实现atoi.go

--- 01_basic/02_variable/11_自己实现atoi.go
+++ 01_basic/02_variable/11_自己实现atoi.go
@@ -5,7 +5,13 @@ import "fmt"
 func myAtoi(str string) int {
 
 	var num int = 0
-	for i := 0; i < len(str); i++ {
+	i := 0
+	flg := true
+	if str[0] == '-' {
+		flg = false
+		i++
+	}
+	for ; i < len(str); i++ {
 		if '0' <= str[i] && str[i] <= '9' {
 			fmt.Println(str[i])
 			fmt.Printf("%T\n", str[i])
@@ -14,12 +20,14 @@ func myAtoi(str string) int {
 			num += int(str[i] - '0')
 		}
 	}
-
+	if !flg {
+		return 0 - num
+	}
 	return num
 }
 
 func main() {
-	str := "0123405"
+	str := "-0123405"
 
 	num := myAtoi(str)
 	fmt.Println(num)
